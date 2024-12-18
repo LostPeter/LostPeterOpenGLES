70cfe70fdbbc45612ea4c6ffc95b5eb9df359888
LostPeter <douzi0630@sohu.com>
2024-12-18 21:58:46 +0800

    Update LostPeterOpenGLES:  Shaders, .sh Script

--- Bin/Shader/Build_MacOS_Android_Shader_GLSL.sh
+++ Bin/Shader/Build_MacOS_Android_Shader_GLSL.sh
@@ -14,5 +14,22 @@ folder_shader=${2}
 
 folderSrc="./glsl/sample"
 folderDst="../Assets/Shader"
+fileDst=$folderDst/$folder_shader/$name_shader.spv
 
-cp -rf $folderSrc/$name_shader $folderDst/$folder_shader/$name_shader.spv
+cp -rf $folderSrc/$name_shader $fileDst
+
+version=#version
+lineSkip=0
+while IFS= read -r line || [[ -n ${line} ]]
+do
+    # echo "$line"
+    if [[ $line =~ $version ]]; then
+        break
+    fi
+    ((lineSkip++))
+done < "$fileDst"
+# echo "$version is in line: $lineSkip"
+
+start=1
+end=$lineSkip
+sed -i "" "${start},${end}"'d' $fileDst
